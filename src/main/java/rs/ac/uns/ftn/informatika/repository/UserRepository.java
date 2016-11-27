b5007ff704149f6c8a8440bcc98a21247058569f
Dalibor Pavicic <dalibor-pavicic@hotmail.com>
2016-11-27 16:23:58 +0100

    Remove classes related to the user

--- src/main/java/rs/ac/uns/ftn/informatika/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package rs.ac.uns.ftn.informatika.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import rs.ac.uns.ftn.informatika.entity.User;
-
-public interface UserRepository extends CrudRepository<User, Long> {
-
-    User findByEmail(String email);
-}

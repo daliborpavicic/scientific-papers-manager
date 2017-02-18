9468c93b1e400c56cece8b8f3c4e5305d1599960
Dalibor Pavicic <dalibor-pavicic@hotmail.com>
2017-02-18 15:45:44 +0100

    Allow an access to API from all origins. Reinitialize storage on app start

--- src/main/java/rs/ac/uns/ftn/informatika/Application.java
+++ src/main/java/rs/ac/uns/ftn/informatika/Application.java
@@ -8,9 +8,13 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import rs.ac.uns.ftn.informatika.configuration.ElasticsearchConfiguration;
 import rs.ac.uns.ftn.informatika.configuration.StorageProperties;
+import rs.ac.uns.ftn.informatika.service.StorageService;
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
@@ -18,14 +22,29 @@ import rs.ac.uns.ftn.informatika.configuration.StorageProperties;
 public class Application {
 	
 	private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+	public static final String NAME = "scientific-papers-manager";
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
+
 	@Bean
-	CommandLineRunner init() {
+	CommandLineRunner init(StorageService storageService) {
 		return (args) -> {
+			storageService.deleteAll();
+            storageService.init();
+            
 			logger.info("scientific-papers-manager is up and running...");
 		};
 	}

fa1c51faa1d4ab79ddcd4f9896b9fe4da1492c5e
Dalibor Pavicic <dalibor-pavicic@hotmail.com>
2017-03-04 22:30:46 +0100

    Remove dummy test

--- src/main/js/src/test-index.js
+++
@@ -1,8 +0,0 @@
-import test from 'ava';
-
-test('my passing test', (t) => {
-  const expected = true;
-  const actual = true;
-
-  t.is(actual, expected, 'actual value should be the same as expected');
-});

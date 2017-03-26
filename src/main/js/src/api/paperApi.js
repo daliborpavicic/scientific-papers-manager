2e0111f39e2cf3dc4ee367914b385e02bffeef7c
Dalibor Pavicic <dalibor-pavicic@hotmail.com>
2017-03-26 11:51:03 +0200

    Add more functions which encapsulate API calls

--- src/main/js/src/api/paperApi.js
+++ src/main/js/src/api/paperApi.js
@@ -1,4 +1,8 @@
-import { postJson, postFormData } from './httpCalls';
+import {
+  get,
+  postJson,
+  postFormData,
+} from './httpCalls';
 import getBaseUrl from './getBaseUrl';
 
 const baseUrl = getBaseUrl();
@@ -10,6 +14,23 @@ export function uploadPaper(paperFile) {
   return postFormData(`${baseUrl}/paper/upload`, formData);
 }
 
+export function getCategories() {
+  return get(`${baseUrl}/paper/categories`);
+}
+
 export function publishPaper(paperMetadata) {
   return postJson(`${baseUrl}/paper/publish`, paperMetadata);
 }
+
+export function searchSimple(query) {
+  return get(`${baseUrl}/paper/search?q=${query}`);
+}
+
+export function searchAdvanced(searchParams) {
+  return postJson(`${baseUrl}/paper/search`, searchParams);
+}
+
+export function searchMoreLikeThis(paperId) {
+  return get(`${baseUrl}/paper/search?paperId=${paperId}`);
+}
+

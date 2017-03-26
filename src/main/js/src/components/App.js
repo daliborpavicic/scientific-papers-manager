843a0842e4f01f1c020d1c1f4641ceab8c6219fc
Dalibor Pavicic <dalibor-pavicic@hotmail.com>
2017-03-26 11:51:16 +0200

    Add navigation links

--- src/main/js/src/components/App.js
+++ src/main/js/src/components/App.js
@@ -2,19 +2,22 @@ import React, { PropTypes } from 'react';
 import { observer, Provider } from 'mobx-react';
 import {
   HashRouter as Router,
-  Route
+  Route,
+  Link
 } from 'react-router-dom';
 import {
   authStore,
+  selectOptionsStore,
   publishPaperStore,
   searchPapersStore
 } from '../state';
+import Protected from './security/Protected';
 import LoginPage from './auth/LoginPage';
 import PublishPaperPage from './paper/publish/PublishPaperPage';
 import SearchPapersPage from './paper/search/SearchPapersPage';
 
 const stores = {
-  authStore, publishPaperStore, searchPapersStore
+  authStore, selectOptionsStore, publishPaperStore, searchPapersStore
 };
 
 window.state = stores;
@@ -26,10 +29,17 @@ const App = () => {
       <Provider {...stores}>
         <div>
           <h1>Scientific papers manager</h1>
+          <ul>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/search'>Search</Link></li>
+            <li><Link to='/publish'>Publish</Link></li>
+            <li><Link to='/protected'>Protected</Link></li>
+          </ul>
           <Route exact path={'/'} render={() => <p>Root - /</p>} />
           <Route exact path={'/login'} component={LoginPage} />
           <Route exact path={'/publish'} component={PublishPaperPage} />
           <Route exact path={'/search'} component={SearchPapersPage} />
+          <Route exact path={'/protected'} component={Protected(() => <div>Protected route</div>)} />
         </div>
       </Provider>
     </Router>
